5b767ca7517f220485da4b89e6ea038725123d1d
Matthew Falbo <matthew.falbo@gmail.com>
2014-04-16 13:39:52 -0600

    Functional Part1 and Part2

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
 $(document).on('ready', function() {
 
-	$(document).on('click',function(e){
+	$('.img').on('click',function(e){
 
 		console.log(e);
 		console.log($(this));
 		var posX = e.pageX;
 		var posY = e.pageY;
 
-		$('.container').children().each(function(i,elem){
-
-			var elemTop = $(elem).css('top');
-			var elemLeft = $(elem).css('left');	
-
-			if((elemTop >= posY && elemTop <= posY+50) && (elemTop >= posX && elemTop <= posX+50)){
-
-				$(elem).remove();
-			}
-			else{
 				var pointer = $('<div class="pointer"></div>');
 				pointer.css('position','absolute').css('top', e.pageY-25).css('left', e.pageX-25);
 				$('.container').append(pointer);
 				console.log(e.pageX, e.pageY);
-			}
-		});
 
+	});
 
+	$('.container').on('click','.pointer', function(){
+
+			$(this).remove();
+			
 	});
 
   
